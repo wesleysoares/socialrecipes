7e4e448968906d23b1e3db51099c157ee7e2373f
Sandro e Wesley <foxhound@campuscode.com>
2015-11-11 19:46:46 -0200

    configurei view

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,8 +5,8 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+  config.wrappers :default, class: 'form-group',
+    hint_class: :field_with_hint, error_class: 'has-error' do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
@@ -151,7 +151,7 @@ SimpleForm.setup do |config|
   # config.cache_discovery = !Rails.env.development?
 
   # Default class for inputs
-  # config.input_class = nil
+  config.input_class = 'form-control'
 
   # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = 'checkbox'
