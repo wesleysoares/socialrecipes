daef8c7d7afc774acd4f914fdfa7a1b2449fff9a
Wesley Soares <wesley.ssoares1@gmail.com>
2016-01-12 22:16:58 -0200

    Modifiquei os testes e implementei novo cenário

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recipes#index'
   resources :recipes, only: [:new, :create, :show]
+  get 'recipes/:id', to:'recipes#show', as: 'recipes_user'
   resources :kitchens, only: [:new, :create, :show]
   resources :type_foods, only: [:new, :create, :show]
   resources :food_preferences, only: [:new, :create, :show]
--- spec/features/user_authenticates_keep_his_recipes_spec.rb
+++ spec/features/user_authenticates_keep_his_recipes_spec.rb
@@ -3,8 +3,29 @@ require 'rails_helper'
 feature 'User keep his recipes' do
   scenario 'and see' do
 
+    user1 = create(:user)
+    user2 = create(:user, email: 'teste@email.com')
+    recipe1 = create(:recipe, user: user1)
+    recipe2 = create(:recipe, user: user1)
+    recipe3 = create(:recipe, user: user2)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user1.email
+    fill_in 'Password', with: user1.password
+
+    click_on 'Log in'
+
+    click_on 'Minhas receitas'
+
+    expect(page).to have_content recipe1.name_recipe
+    expect(page).to have_content recipe2.name_recipe
+    expect(page).not_to have_content recipe3.name_recipe
+  end
+
+  scenario 'and not recipes' do
+
     user = create(:user)
-    recipe = create(:recipe, user: user)
 
     visit new_user_session_path
 
@@ -15,6 +36,6 @@ feature 'User keep his recipes' do
 
     click_on 'Minhas receitas'
 
-    expect(page).to have_content recipe.name_recipe
+    expect(page).to have_content 'Sem receitas cadastradas.'
   end
 end
