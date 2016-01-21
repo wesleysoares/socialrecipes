24eb932201957972826228f6decf6b105db9235f
Wesley Soares <wesley.ssoares1@gmail.com>
2016-01-20 23:57:42 -0200

    Modifiquei layout da tela de receitas por usuário

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -14,7 +14,7 @@ class Recipe < ActiveRecord::Base
             :preparation,
             presence: true
 
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :image, styles: { medium: '200x200>', thumb: '100x100>' }
   validates_attachment_content_type :image,
                                     content_type: ['image/jpeg',
                                                    'image/gif',
--- spec/features/user_authenticates_keep_his_recipes_spec.rb
+++ spec/features/user_authenticates_keep_his_recipes_spec.rb
@@ -6,8 +6,7 @@ feature 'User keep his recipes' do
     user1 = create(:user)
     user2 = create(:user, email: 'teste@email.com')
     recipe1 = create(:recipe, user: user1)
-    recipe2 = create(:recipe, user: user1)
-    recipe3 = create(:recipe, user: user2)
+    recipe2 = create(:recipe, user: user2)
 
     visit new_user_session_path
 
@@ -19,8 +18,10 @@ feature 'User keep his recipes' do
     click_on 'Minhas receitas'
 
     expect(page).to have_content recipe1.name_recipe
-    expect(page).to have_content recipe2.name_recipe
-    expect(page).not_to have_content recipe3.name_recipe
+    expect(page).to have_content recipe1.kitchen.name
+    expect(page).to have_content recipe1.type_food.name
+    expect(page).to have_content recipe1.food_preference.name
+    expect(page).not_to have_content recipe2.name_recipe
   end
 
   scenario 'and not recipes' do
@@ -37,5 +38,32 @@ feature 'User keep his recipes' do
     click_on 'Minhas receitas'
 
     expect(page).to have_content 'Sem receitas cadastradas :('
+  end
+
+  scenario 'and see details of recipe' do
+
+    user = create(:user)
+    recipe = create(:recipe, user: user)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log in'
+
+    click_on 'Minhas receitas'
+    click_on 'Bolo de chocolate'
+
+    expect(page).to have_content('Bolo de chocolate')
+    expect(page).to have_content recipe.kitchen.name
+    expect(page).to have_content recipe.type_food.name
+    expect(page).to have_content recipe.food_preference.name
+    expect(page).to have_content('5')
+    expect(page).to have_content('60')
+    expect(page).to have_content('Médio')
+    expect(page).to have_content('Farinha de trigo e fermento')
+    expect(page).to have_content('Junte tudo e pronto!')
+    expect(page).to have_xpath("//img[contains(@src,'bolo.jpg')]")
   end
 end
